a1b6ff1d269db179011094356072f6775dc645f7
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-07-26 03:05:48 +0000

    機能復習

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@ Rails.application.routes.draw do
   
   get 'homes/top' => "homes#top"
   
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'list/:id' => 'lists#update', as: 'update_list'
+  resources :lists
 end
